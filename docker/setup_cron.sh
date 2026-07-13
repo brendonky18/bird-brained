9794b5e25fbd2108f3be5f84170aa600bd6bf705
Brendon Ky <brendonky18@gmail.com>
2026-07-12 20:36:34 -0400

    update dockerfile and docker compose

--- docker/setup_cron.sh
+++ docker/setup_cron.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 : "${REFRESH_INTERVAL="0 0 * * *"}" # default to once a day at midnight
-: "${EBIRD_REGION="world"}" # default to all sightings
+: "${EBIRD_REGION="MAJOR_REGION:world"}" # default to all sightings
 set -ef
 echo "using region: $EBIRD_REGION"
 echo "using refresh interval: $REFRESH_INTERVAL"
-echo "$REFRESH_INTERVAL /usr/local/bin/python3 -m bird_brained --region $EBIRD_REGION --headless > /proc/1/fd/1 2>&1" >> /etc/cron.d/brid_brained
+echo "$REFRESH_INTERVAL /usr/local/bin/python3 -m bird_brained --location $EBIRD_REGION --headless > /proc/1/fd/1 2>&1" >> /etc/cron.d/brid_brained
 
 printenv >> /etc/environment
 
