70a5b663f077c7397d783700323f6cac96c8d0d9
Brendon Ky <brendonky18@gmail.com>
2026-07-13 17:00:01 -0400

    update container and docker compose

--- docker/setup_cron.sh
+++ docker/setup_cron.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 : "${REFRESH_INTERVAL="0 0 * * *"}" # default to once a day at midnight
-: "${EBIRD_REGION="MAJOR_REGION:world"}" # default to all sightings
+: "${EBIRD_LOCATION="MAJOR_REGION:world"}" # default to all sightings
 set -ef
-echo "using region: $EBIRD_REGION"
+echo "using location: $EBIRD_LOCATION"
 echo "using refresh interval: $REFRESH_INTERVAL"
-echo "$REFRESH_INTERVAL /usr/local/bin/python3 -m bird_brained --location $EBIRD_REGION --headless > /proc/1/fd/1 2>&1" >> /etc/cron.d/brid_brained
+echo "$REFRESH_INTERVAL /usr/local/bin/python3 -m bird_brained --location $EBIRD_LOCATION --headless > /proc/1/fd/1 2>&1" >> /etc/cron.d/brid_brained
 
 printenv >> /etc/environment
+
+/usr/local/bin/python3 -m bird_brained --location $EBIRD_LOCATION --headless
 
 crontab /etc/cron.d/brid_brained
 echo "Finished setup, starting cron daemon in foreground"
